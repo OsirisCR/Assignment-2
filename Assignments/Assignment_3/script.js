9339c3d2a76a72c2e193d8764d1f6c67d2e4b3dd
OsirisCR <87344538+OsirisCR@users.noreply.github.com>
2024-03-09 12:44:00 -0500

    Add files via upload

---
+++ Assignments/Assignment_3/script.js
@@ -0,0 +1,12 @@
+/* document.querySelector(".main-img").onclick = () => {
+  document.querySelector(".popup-img").style.display = "block";
+};
+
+document.querySelector(".popup-img span").onclick = () => {
+  document.querySelector(".popup-img").style.display = "none";
+}; */
+
+function togglePopUp() {
+  /* console.log("nofunoncia"); */
+  document.getElementById("popup-img").classList.toggle("active");
+}
